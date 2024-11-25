65d1636f7dd59f3d2300711c07533b54967312d2
jyothikaogeti <ogetijyothika2002@gmail.com>
2024-11-25 15:03:28 +0530

    performed data validation

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig, TrainingPipelineConfig
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 
 
 if __name__ == "__main__":
@@ -11,7 +12,15 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(data_ingestion_config)
         logging.info("Initiate the Data Ingestion")
         data_ingestion_artifact = data_ingestion.initialize_data_ingestion()
+        logging.info("Data Initialization Completed")
         print(data_ingestion_artifact)
+
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate the Data Validation")
+        data_validation_artifact = data_validation.initialize_data_validation()
+        logging.info("Data Validation Completed")
+        print(data_validation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, SystemError)
--- networksecurity/constants/__init__.py
+++ networksecurity/constants/__init__.py
@@ -10,6 +10,7 @@ TARGET_COLUMN = "Result"
 PIPELINE_NAME: str = "NetworkSecurity"
 ARTIFACT_DIR: str = "Artifacts"
 FILE_NAME: str = "phisingData.csv"
+SCHEMA_FILE_PATH = "schema.yaml"
 
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
@@ -24,3 +25,13 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested_data"
 DATA_INGESTION_TRAIN_TEST_TEST_SPLIT_RATIO: float = 0.2
+
+
+"""
+Data Validation related constants start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR:str = "validated_data"
+DATA_VALIDATION_INVALID_DIR:str = "invalid_data"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = "data_drift_report.yaml"
